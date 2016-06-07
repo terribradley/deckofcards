f37fa9c373cd943555a91ca59f878ed50e91c08d
Carl Egbert and Terri Bradley <terri@>
2016-06-07 10:09:50 -0700

    make this branch use for instead of forEach

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,27 @@ $(function () {
   var cardValues = ['A',"2","3",'4','5','6','7','8','9','10','J','Q','K'];
   var cardSuits = ["Clubs","Spades","Diamonds","Hearts"];
   var cardDeck = [];
-  cardSuits.forEach(function(cardSuit){
-    cardValues.forEach(function(cardValue){
-      cardDeck.push(cardValue+" of "+cardSuit);
-    });
-  });
-  cardDeck.forEach(function(card){
-    document.write("<li>"+card+"</li>");
-  });
+
+  for (var suitIndex = 0 ; suitIndex < cardSuits.length ; suitIndex ++) {
+    for (var valueIndex = 0 ;  valueIndex < cardValues.length ; valueIndex ++) {
+      cardDeck.push(cardValues[valueIndex]+" of "+cardSuits[suitIndex]);
+    }
+  }
+
+  for (var deckIndex =0 ; deckIndex < cardDeck.length ; deckIndex ++) {
+    document.write(deckIndex);
+    document.write("<li>"+cardDeck[deckIndex]+"</li>");
+  }
+
+
+
+
+  // cardSuits.forEach(function(cardSuit){
+  //   cardValues.forEach(function(cardValue){
+  //     cardDeck.push(cardValue+" of "+cardSuit);
+  //   });
+  // });
+  // cardDeck.forEach(function(card){
+  // //   document.write("<li>"+card+"</li>");
+  // });
 });
